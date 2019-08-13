da2dd25095a595bfcc129945cc4c65feb94a14f7
Douglas E. Rogers <hitupdoug+github@gmail.com>
2019-08-13 10:20:27 -0700

    initial commit

---
+++ src/pages/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Link } from 'gatsby'
+
+import Layout from '../components/layout'
+import Image from '../components/image'
+
+const IndexPage = () => (
+  <Layout>
+    <h1>Hi</h1>
+    <p>I'm Doug. Co-founder / CTO of <a href="https://www.prettyinstant.com">Pretty Instant, Inc</a></p>
+    <Link to="/about/">About</Link>
+  </Layout>
+)
+
+export default IndexPage
